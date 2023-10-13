3661adc20c1835e5e25ef742fdb592c5e5370bf5
Hilmi Berkay Ozdemir <59442397+hberkayozdemir@users.noreply.github.com>
2023-10-13 14:29:24 +0300

    initial commit

---
+++ src/main/java/com/finswipe/todo/LearnToDoAppWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.finswipe.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnToDoAppWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnToDoAppWithSpringBootApplication.class, args);
+	}
+
+}
